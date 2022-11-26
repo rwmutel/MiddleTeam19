3eb51c6105a8c25bbd5c0e8c96aac658b3e34acd
Roman <romanmutel@gmail.com>
2022-11-26 18:02:23 +0200

    initial commit

---
+++ src/main/java/ua/edu/ucu/apps/MiddleTeam19/MiddleTeam19Application.java
@@ -0,0 +1,13 @@
+package ua.edu.ucu.apps.MiddleTeam19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleTeam19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddleTeam19Application.class, args);
+	}
+
+}
